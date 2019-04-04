7a0070d2b76e8e69400b6b82879cd27e41d29aec
Wai Chun Leung <725030@student.tor.loc>
2019-04-04 16:37:40 -0400

    4:35 Added Submit Button

--- C0725030/PetersWebForm.aspx
+++ C0725030/PetersWebForm.aspx
@@ -12,6 +12,10 @@
             <h1>Newsletter Signup</h1>
         </div>
         <asp:Label ID="lblEmail" runat="server" Text="Email Peter"></asp:Label>
+        <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Button ID="btnSubmit" runat="server" Text="Submit" />
     </form>
 </body>
 </html>
--- C0725030/PetersWebForm.aspx.designer.cs
+++ C0725030/PetersWebForm.aspx.designer.cs
@@ -22,12 +22,30 @@ namespace C0725030 {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// Label1 control.
+        /// lblEmail control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label lblEmail;
+        
+        /// <summary>
+        /// txtEmail control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmail;
+        
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSubmit;
     }
 }
